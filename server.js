39cce5d2dbc0a69fe1f5e29764c1a870552a7bad
Syzole <championarjun2004@gmail.com>
2024-02-21 17:00:26 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const ytdl = require('ytdl-core');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+const downloadDirectory = path.join(__dirname, 'downloads'); // Specify your desired download directory
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'public', 'index.html'));
+});
+
+//this was for testing
+app.get('/download', (req, res) => {
+    const videoUrl = 'http://www.youtube.com/watch?v=aqz-KE-bpKQ';
+    const videoStream = ytdl(videoUrl, { quality: 'highest' });
+
+    const filePath = path.join(downloadDirectory, 'video.mp4');
+    const fileWriteStream = fs.createWriteStream(filePath);
+
+    videoStream.pipe(fileWriteStream);
+
+    fileWriteStream.on('finish', () => {
+        res.setHeader('Content-Disposition', `attachment; filename="video.mp4"`);
+        res.sendFile(filePath, {}, () => {
+            fs.unlinkSync(filePath); // Delete the file after it has been sent
+            alert("Download Complete");
+        });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
