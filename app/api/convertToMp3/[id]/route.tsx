83edc7f2a7ebcf3341f16bd4e55ef2e5a24b1c0c
Syzole <108027056+Syzole@users.noreply.github.com>
2024-06-01 04:34:57 -0400

    chore: Refactor convertToMp3 route and add downloadMP3 endpoint

--- app/api/convertToMp3/[id]/route.tsx
+++ app/api/convertToMp3/[id]/route.tsx
@@ -1,6 +1,6 @@
 "use server";
 
-import { NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
 import ytdl from "ytdl-core";
 import fs, { readFileSync, writeFileSync } from "fs";
@@ -11,7 +11,6 @@ import { ID3Writer } from "browser-id3-writer";
 import axios from "axios";
 import sharp from "sharp";
 import { use } from "react";
-
 
 const directName = path.resolve("./downloads");
 
@@ -24,15 +23,41 @@ if (!fs.existsSync(directName)) {
 	fs.mkdirSync(path.join(directName, "spotify"));
 }
 
-
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-	const  id  = params.id;
-
-	return NextResponse.json({id: id});
+export async function GET(
+	req: Request,
+	{ params }: { params: { id: string } }
+) {
+	const id = params.id;
+	return NextResponse.json({ id: id });
 }
 
-export async function POST(req: Request) {
-	return NextResponse.json({});
-}
+export async function POST(
+	req: Request,
+	{ params }: { params: { id: string } }
+) {
+	const id = params.id;
+	console.log(id);
+	console.log(await req.formData());
 
+	switch (id) {
+		case "downloadMP3":
+			const { url } = await req.json();
+			if (!url) {
+				return NextResponse.json(
+					{ error: "Please enter a URL" },
+					{ status: 400 }
+				);
+			}
+			await downloadMP3(url);
+			return NextResponse.json({ message: "Downloaded MP3" }, { status: 200 });
+		default:
+			console.log("Invalid ID");
+			return NextResponse.json({ message: "Invalid ID" }, { status: 400 });
+	}
+}
 //helper functions go here
+
+async function downloadMP3(url: string) {
+	console.log(url);
+	return "this is more of a placeholder for now";
+}
--- app/ytToMp3/page.tsx
+++ app/ytToMp3/page.tsx
@@ -1,11 +1,65 @@
-'use client';
+"use client";
 
 export default function Page() {
 	return (
-		<div className="flex justify-center">
-			<button className="btn btn-neutral">Test</button>
+		<div className="flex flex-col justify-center p-8">
+			<div className="navbar bg-base-100">
+				<a
+					className="btn btn-ghost text-xl"
+					href="/"
+				>
+					Home
+				</a>
+			</div>
+			<h1 className="font-sans self-center p-8 text-xl">
+				Below in the text box, add a{" "}
+				<a
+					href="https://www.youtube.com/"
+					className="text-blue-600"
+				>
+					{" "}
+					Youtube
+				</a>{" "}
+				URL and it will be converted to an MP3 file ready to downloaded
+			</h1>
+			<input
+				type="text"
+				id="ytUrl"
+				placeholder="Youtube URL"
+				className="input input-md input-primary mb-5"
+			/>
+			<button
+				className="btn btn-accent justify-center mb-4"
+				onClick={Download}
+			>
+				{" "}
+				Download
+			</button>
 		</div>
 	);
 }
 
+async function Test() {
+	let test = await fetch("/api/convertToMp3/test", {
+		method: "GET",
+	});
+	let json = await test.json();
+	console.log(json);
+}
 
+async function Download() {
+	let url = (document.getElementById("ytUrl") as HTMLInputElement).value;
+	console.log(url);
+	if (!url) {
+		alert("Please enter a URL");
+		return;
+	}
+	
+	let response = await fetch("/api/convertToMp3/downloadMP3", {
+		method: "POST",
+		body: JSON.stringify({ url }),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+}
